374817c2eba995df502afc4a040ede143eb51a1a
Ramesh Rasaiyan <ramesh_rasaiyan@cable.comcast.com>
2015-11-06 23:21:05 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Data-Driven Documents - Icons with circle and fontawesome
+
+<a href="http://d3js.org"><img src="http://d3js.org/logo.svg" align="left" hspace="10" vspace="6"></a>
+
+This is a simple example to generate the fontawesome icons with the circles. 
---
+++ js/d3-icons.js
@@ -0,0 +1,75 @@
+/**
+ * Created by rrasai001c on 11/6/15.
+ */
+
+/**
+ * In the below dataSet, facode has been taken from this site http://astronautweb.co/snippet/font-awesome/
+ * Add the 'u' between '\' and 'f' ti make the fontawesome work. Ex:- "\f021" should be "\uf021"
+ */
+
+var dataSet = [
+    { "facode":"\uf021", "position":1, "name":"CTE" },
+    { "facode":"\uf085", "position":2, "name":"PS"},
+    { "facode":"\uf0ac", "position":3, "name":"HSD" }
+]
+
+var width = 800,
+    height = 400;
+
+var svgContainer = d3.select("#svg-container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+var gradient = svgContainer.append("svg:defs")
+    .append("svg:linearGradient")
+    .attr("id", "gradient")
+    .attr("x1", "0%")
+    .attr("y1", "0%")
+    .attr("x2", "100%")
+    .attr("y2", "100%")
+    .attr("spreadMethod", "pad")
+
+gradient.append("svg:stop")
+    .attr("offset", "0%")
+    .attr("stop-color", "#FFFFFF")
+    .attr("stop-opacity", "1");
+
+gradient.append("svg:stop")
+    .attr("offset", "100%")
+    .attr("stop-color", "#dadada")
+    .attr("stop-opacity", "1");
+
+var iconContainer = svgContainer.append("g");
+
+var iconCircle = iconContainer.selectAll("circle")
+    .data(dataSet)
+    .enter().append("circle")
+    .attr("cx", function(d) { return d.position * 55 })
+    .attr("cy", "100")
+    .attr("r", "23")
+    .attr("fill", "url(#gradient)");
+
+iconContainer.selectAll('text')
+    .data(dataSet)
+    .enter().append("text")
+    .attr("font-size", "1.7em")
+    .attr("font-family", "FontAwesome")
+    .attr("x", function(d) { return d.position * 55 })
+    .attr("y", "100")
+    .attr("dy", ".35em")
+    .attr("text-anchor", "middle")
+    .attr("fill", "#6c6c6c")
+    .text(function(d) {  return d.facode });
+
+iconContainer.selectAll("text.name")
+    .data(dataSet)
+    .enter().append("text")
+    .attr("font-size", ".900em")
+    .attr("font-family", "verdana")
+    .attr("x", function(d) { return d.position * 55 })
+    .attr("y", "145")
+    .attr("text-anchor", "middle")
+    .attr("fill", "#6c6c6c")
+    .text(function(d) { return d.name });
+
